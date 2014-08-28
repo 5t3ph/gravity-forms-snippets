7ff78cb1294e0cf80a6c4237d4e652aae720783d
5t3ph <thinkdobecreate@gmail.com>
2014-08-27 21:26:46 -0500

    Adding Bootstrap Field Style Function
    Function adding bootstrap classes for the most common field types.

---
+++ gravity-forms-bootstrap-styles.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Gravity Forms Bootstrap Styles
+ *
+ * Applies bootstrap classes to various common field types.
+ * Requires Bootstrap to be in use by the theme.
+ *
+ * Using this function allows use of Gravity Forms default CSS
+ * in conjuction with Bootstrap (benefit for fields types such as Address).
+ *
+ * @see  gform_field_content
+ * @link http://www.gravityhelp.com/documentation/page/Gform_field_content
+ *
+ * @return string Modified field content
+ */
+add_filter("gform_field_content", "bootstrap_styles_for_gravityforms_fields", 10, 5);
+function bootstrap_styles_for_gravityforms_fields($content, $field, $value, $lead_id, $form_id){
+	
+	// Currently only applies to most common field types, but could be expanded.
+	
+	if($field["type"] != 'hidden' && $field["type"] != 'list' && $field["type"] != 'multiselect' && $field["type"] != 'checkbox' && $field["type"] != 'fileupload' && $field["type"] != 'date' && $field["type"] != 'html' && $field["type"] != 'address') {
+		$content = str_replace('class=\'medium', 'class=\'form-control medium', $content);
+	}
+	
+	if($field["type"] == 'name' || $field["type"] == 'address') {
+		$content = str_replace('<input ', '<input class=\'form-control\' ', $content);
+	}
+	
+	if($field["type"] == 'textarea') {
+		$content = str_replace('class=\'textarea', 'class=\'form-control textarea', $content);
+	}
+	
+	if($field["type"] == 'checkbox') {
+		$content = str_replace('li class=\'', 'li class=\'checkbox ', $content);
+		$content = str_replace('<input ', '<input style=\'margin-left:1px;\' ', $content);
+	}
+	
+	if($field["type"] == 'radio') {
+		$content = str_replace('li class=\'', 'li class=\'radio ', $content);
+		$content = str_replace('<input ', '<input style=\'margin-left:1px;\' ', $content);
+	}
+	
+	return $content;
+	
+} // End bootstrap_styles_for_gravityforms_fields()
+
+?>
